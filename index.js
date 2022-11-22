8dcf42d70f7c4ab458c2ea1fe52667681be7e253
Joel Turner <joelbturner.mb@gmail.com>
2022-11-22 09:56:15 -0500

    commit tree e756bf3d266fb243670086ec9c7c66fee53bc3ae

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+   const names = drivers.slice(0,2)
+   return names;
+    
+}
+
+function returnLastTwoDrivers(drivers){
+   const names = drivers.slice(drivers.length - 2, (drivers.length ))
+    return names;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+function createFareMultiplier(number){
+   
+    return function(multiplier){
+        multiplier * number;
+    }
+}
